a6ebb5be264fbdbb43eb674d8d4463a40321022b
Godwin <godwin.yampoi@student.moringaschool.com>
2023-03-31 10:39:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// remove main#main
+document.querySelector('main#main').remove();
+
+// create new header
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
